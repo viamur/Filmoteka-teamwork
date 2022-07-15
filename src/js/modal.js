a9730043f929d3a6090085698f3c3346f69806e5
Moskalenko Serhii <mynameserhii@gmail.com>
2022-07-15 10:12:15 +0300

    apitest

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,11 @@
 import { ApiFetchId } from './api-fetch-id';
 import makeModal from '../partials/hbs/modal.hbs';
+import * as basicLightbox from 'basiclightbox';
 
 const fetchId = new ApiFetchId();
 
-const div = document.querySelector('.js-test');
+const div = document.querySelector('.js-modal-close');
 
-const tex = fetchId.apiFetchId(453395).then(data => {
-  div.innerHTML = makeModal(data);
+fetchId.apiFetchId(453395).then(data => {
+  const ca = basicLightbox.create(makeModal(data)).show();
 });
