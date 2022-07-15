18a17a19830554c9e89cb8584fa719de61946f0c
Moskalenko Serhii <mynameserhii@gmail.com>
2022-07-15 12:46:47 +0300

    te

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,6 +6,6 @@ const fetchId = new ApiFetchId();
 
 const div = document.querySelector('.js-modal-close');
 
-fetchId.apiFetchId(453395).then(data => {
-  const ca = basicLightbox.create(makeModal(data)).show();
-});
+// fetchId.apiFetchId(453395).then(data => {
+//   const ca = basicLightbox.create(makeModal(data)).show();
+// });
