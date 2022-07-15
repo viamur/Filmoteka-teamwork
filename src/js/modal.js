8fee17dcd4ff72c851cf0b36925ea6a12b8eb016
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-15 18:48:52 +0300

    update

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,16 @@
 import { ApiFetchId } from './api-fetch-id';
 import makeModal from '../partials/hbs/modal.hbs';
 import * as basicLightbox from 'basiclightbox';
+import makeGallery from '../partials/hbs/search-film-card.hbs';
 
 const fetchId = new ApiFetchId();
 const list = document.querySelector('.library__list');
-
-const onOpenModal = e => {
-  e.preventDefault();
-  console.log(e.target.closest('a'));
+fetchId.page = 1;
+const fetchAndMake = () => {
+  fetchId.trandFetch().then(data => {
+    console.log(data);
+    list.innerHTML = makeGallery(data);
+  });
 };
 
-list.addEventListener('click', onOpenModal);
+fetchAndMake();
