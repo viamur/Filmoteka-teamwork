ad28317a08d01c685890abfc9152f77838ead22e
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-07-16 12:37:11 +0300

    pagin

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
   renderQueueList,
 } from './js/render-list';
 import { workLocStorage } from './js/local-storage';
+import './js/pagination';
 
 if (
   workLocStorage.getUserLocationPage() === undefined ||
@@ -13,10 +14,14 @@ if (
 ) {
   /* функция, отрисовка главной страницы трендовые фильмы */
   rederTrandList();
-} else if (workLocStorage.getUserLocationPage() === workLocStorage.VALUE_QUEUE) {
+} else if (
+  workLocStorage.getUserLocationPage() === workLocStorage.VALUE_QUEUE
+) {
   /* функиця, отрисовка вкладки QUEUE */
   renderQueueList();
-} else if (workLocStorage.getUserLocationPage() === workLocStorage.VALUE_WATCHED) {
+} else if (
+  workLocStorage.getUserLocationPage() === workLocStorage.VALUE_WATCHED
+) {
   /* функция, отрисовки вкладки  WATCHED */
   renderWatchedList();
 } else {
