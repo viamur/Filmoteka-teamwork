c27d12a56dbda2c9ce7b0d667fb9d1a428dc98db
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-16 22:09:29 +0300

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
+import { renderSearchList } from './render-list';
 import Notiflix from 'notiflix';
-import { searchFetch } from './api-fetch-id';
-import moduleName from './render-list';
-import cardTpl from '../partials/hbs/library-film-card.hbs';
 
 const formRef = document.querySelector('.js-search-form');
-const inputRef = document.querySelector('.js-search-input');
-const searchBtn = document.querySelector('.js-search-btn');
 
 const gallery = document.querySelector('.library__list');
 
-const fetchApi = new ApiFetchId();
+function searchHandler(e) {
+  e.preventDefault();
+  const { searchQuery } = e.target.elements;
+  console.log('searchQuery.value :>> ', searchQuery);
+  /*  if (searchQuery.value.length > 1) {
+    console.log('searchQuery.value :>> ', searchQuery);
+    return searchQuery;
+    console.log('searchQuery.value :>> ', searchQuery.value);
+  } */
+}
 
-const page = 1;
-/* const query = '';
-
-query = document.getElementById('inputRef').value; */
-
-formRef.addEventListener('submit', renderSearchList('thor'));
+formRef.addEventListener('submit', renderSearchList());
