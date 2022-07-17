0f70ae721e8858e8ebbf102848406bb195dd64c7
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-17 18:51:26 +0300

    home library btns js

---
+++ src/js/home-library-btn.js
@@ -0,0 +1,6 @@
+const refs = {
+  homeBtn: document.querySelector('.js-home-btn'),
+  libraryBtn: document.querySelector('.js-nav-btn'),
+  watchedBtn: document.querySelector('.js-watched-btn'),
+  queueBtn: document.querySelector('.js-queue-btn'),
+};
