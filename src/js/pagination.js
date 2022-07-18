822466b24a3172327b4d759f60c1ee46b51b56d6
Moskalenko Serhii <mynameserhii@gmail.com>
2022-07-19 00:07:55 +0300

    fix add 4 pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,7 +9,7 @@ import {
 
 const container = document.querySelector('.tui-pagination');
 
-const pagination = new Pagination(container, {
+const options = {
   itemsPerPage: 20,
   visiblePages: 5,
   centerAlign: true,
@@ -21,7 +21,11 @@ const pagination = new Pagination(container, {
     disabledMoveButton: '<button class="move-btn move-btn-{{type}} disabled" disabled></button>',
     moreButton: '<a class="page-btn next-is-ellip last-child">...</a>',
   },
-});
+};
+const pagination = new Pagination(container, options);
+const paginationA = new Pagination(container, options);
+const paginationB = new Pagination(container, options);
+const paginationC = new Pagination(container, options);
 // пагинация по инпуту
 async function fetchPerPageSearch(page) {
   api.page = page;
@@ -40,34 +44,46 @@ export async function searchInput(query) {
 
   pagination.reset(api.totalItems);
 }
-// пагинация по тренду
+// пагинация по тренду paginationA
 async function fetchPerPageTrand(page) {
   api.page = page;
   await rederTrandList();
 }
 export async function searchTrand() {
-  pagination.setItemsPerPage(20);
+  paginationA.setItemsPerPage(20);
   api.page = 1;
 
-  pagination.on('afterMove', event => {
+  paginationA.on('afterMove', event => {
     const currentPage = event.page;
     fetchPerPageTrand(currentPage);
   });
   await fetchPerPageTrand(1);
 
-  pagination.reset(api.totalItems);
+  paginationA.reset(api.totalItems);
 }
 
-function forWatchedAndQueue() {
+function forWatchedAndQueueB() {
   const windowWidthPag = window.innerWidth;
   if (windowWidthPag < 768) {
-    pagination.setItemsPerPage(4);
+    paginationB.setItemsPerPage(4);
   }
   if (windowWidthPag >= 768 && windowWidthPag < 1280) {
-    pagination.setItemsPerPage(8);
+    paginationB.setItemsPerPage(8);
   }
   if (windowWidthPag >= 1280) {
-    pagination.setItemsPerPage(9);
+    paginationB.setItemsPerPage(9);
+  }
+}
+function forWatchedAndQueueC() {
+  const windowWidthPag = window.innerWidth;
+  if (windowWidthPag < 768) {
+    paginationC.setItemsPerPage(4);
+  }
+  if (windowWidthPag >= 768 && windowWidthPag < 1280) {
+    paginationC.setItemsPerPage(8);
+  }
+  if (windowWidthPag >= 1280) {
+    paginationC.setItemsPerPage(9);
   }
 }
 // пагинация по queue
@@ -76,16 +92,16 @@ async function fetchPerPageQueue(page) {
   await renderQueueList();
 }
 export async function searchQueue() {
-  forWatchedAndQueue();
+  forWatchedAndQueueB();
   api.page = 1;
 
-  pagination.on('afterMove', event => {
+  paginationB.on('afterMove', event => {
     const currentPage = event.page;
     fetchPerPageQueue(currentPage);
   });
   await fetchPerPageQueue(1);
 
-  pagination.reset(api.totalItems);
+  paginationB.reset(api.totalItems);
 }
 
 // пагинация по Watched
@@ -94,14 +110,14 @@ async function fetchPerPageWatched(page) {
   await renderWatchedList();
 }
 export async function searchWatched() {
-  forWatchedAndQueue();
+  forWatchedAndQueueC();
   api.page = 1;
 
-  pagination.on('afterMove', event => {
+  paginationC.on('afterMove', event => {
     const currentPage = event.page;
     fetchPerPageWatched(currentPage);
   });
   await fetchPerPageWatched(1);
 
-  pagination.reset(api.totalItems);
+  paginationC.reset(api.totalItems);
 }
