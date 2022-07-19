1222bc622484316765b3c1edfef8a7a5b2dd4992
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-07-19 17:05:19 +0300

    fix mathe fucka)

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,25 +5,19 @@ const Theme = {
 
 const checkBox = document.querySelector('.theme-switch__toggle');
 checkBox.addEventListener('change', onCheked);
-const footer = document.querySelector('.footer');
-const header = document.querySelector('.header');
-const modal = document.querySelector('.modal');
-console.log(modal);
+
 function chekLocalstorage() {
   if (localStorage.getItem('cheked') === 'true') {
     checkBox.checked = true;
     document.body.classList.add('dark-theme');
-    footer.classList.add('dark-theme');
   }
 }
 chekLocalstorage();
 
 function onCheked(event) {
   if (checkBox.checked) {
-    header.classList.add('dark-theme');
     document.body.classList.add('dark-theme');
-    footer.classList.add('dark-theme');
-    footer.classList.remove('light-theme');
+
     localStorage.setItem('Theme', 'DARK');
     localStorage.setItem('cheked', 'true');
     checkBox.checked = true;
@@ -31,9 +25,7 @@ function onCheked(event) {
   }
 
   document.body.classList.remove('dark-theme');
-  footer.classList.remove('dark-theme');
-  header.classList.remove('dark-theme');
-  footer.classList.add('light-theme');
+
   localStorage.setItem('Theme', 'LIGHT');
   localStorage.setItem('cheked', 'false');
 }
